5f132676dc9bf34b88bef8680e0a6ee8024b18db
nabusim <cristianjose@gmail.com>
2024-08-22 21:49:46 +0200

    Fix thema persistencia y mas reestructuracion

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
+export const AUTHOR = 'Cristian J. Perez Guerra';
+export const HEAD_TITLE = 'nabusim website';
 export const HOME_TITLE = 'Nabusim website | Home';
 export const ABOUT_TITLE = 'Nabusim website | About';
-export const SITE_DESCRIPTION = 'Bienvenido al Website de Cristian Jose Perez Guerra';
+export const SITE_DESCRIPTION = `Bienvenido al Website de ${AUTHOR}`;
 export const PROJECT_PAGE = 'Proyectos';
 export const ABOUT_PAGE = 'Acerca de...';

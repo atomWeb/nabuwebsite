b3f1bd4897e5dc457287c197a2a18db83a5c3daa
nabusim <cristianjose@gmail.com>
2024-08-22 20:55:22 +0200

    mejoras estructura sitio

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+export const HOME_TITLE = 'Nabusim website | Home';
+export const ABOUT_TITLE = 'Nabusim website | About';
+export const SITE_DESCRIPTION = 'Bienvenido al Website de Cristian Jose Perez Guerra';
+export const PROJECT_PAGE = 'Proyectos';
+export const ABOUT_PAGE = 'Acerca de...';
